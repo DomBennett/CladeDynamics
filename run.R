8ffbbe35dd3decb4e9ccd75573092eecca008894
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-07-14 14:06:51 +0100

    Changed parameters

--- run.R
+++ run.R
@@ -4,8 +4,8 @@
 
 ## Model Parameters
 seed.n <- 2 # how big should the initial tree be?
-time <- 10 # how many units of branch length should the tree grow by?
-sample <- 0.1 # how often should sampling the success of a tree occur?
+time <- 5 # how many units of branch length should the tree grow by?
+sample <- 0.01 # how often should sampling the success of a tree occur?
 birth <- 1.1 # how many births to deaths per unit of branch length?
 death <- 1
 bias <- 'none' # 'none', 'PE' or 'FP'
