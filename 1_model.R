da7c7ee51bd1700a8694502be3d3910e247b87d1
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-07-18 22:10:50 +0100

    Re-worked for new model functions

--- 1_model.R
+++ 1_model.R
@@ -1,73 +1,63 @@
 ## 17/06/2014
 ## D.J. Bennett
-## The rise and fall of clades: here I use
-##  an equal rates markov model to add and remove
-##  species to a tree. I then record the success
-##  of each clade at set time intervals.
-## N.B. It may crash unexpetedly as
-##  all clades may have gone extinct; re-run
+## Modelling tree growth using a modified rates
+## markov model, where rates are determined by ED.
+## This script is run through run.R
 
 ## Libraries
 source (file.path ('tools', 'model_tools.R'))
 
 ## Dirs
-res.dir <- paste0 ("t", time, 's', sample,
-                   'b', round (birth), 'd', round (death),
-                   'sn', seed.n, bias, '_',
-                   format(Sys.time(), "%H%M_%a%b%d%Y"))
-write.table (res.dir, file.path ('results', 'run_log.txt'),
-             append = TRUE, row.names = FALSE, col.names = FALSE)
+# create a unique dir based on time for every run
+res.dir <- format (Sys.time (), "%H%M_%d%m%y")
+# record parameters in runlog
+parameters <- data.frame (res.dir, strength, bias, time,
+                          sample, birth, death, seed.n,
+                          min.time.span, min.size)
+write.table (parameters, runlog, sep = ',', append = TRUE,
+             col.names = FALSE, row.names = FALSE)
+# set res.dir's path
 res.dir <- file.path ('results', res.dir)
 if (!file.exists (res.dir)) {
   dir.create (res.dir)
 }
 
-## Globals
-# count nodes and tips, necessary for adding new nodes
-# otherwise would not be unique across time steps
+## Set-up
+# globals: count nodes and tips, necessary for adding new nodes
+#  otherwise would not be unique across time steps
 max.node <- seed.n - 1
 max.tip <- seed.n # starting tree has n tips and n-1 int node
 extinct <- c () # vector of all extinct species
-
-## Set-up
 # seed tree age is the same as a sample unit
 tree <- seedTree (seed.n, sample)
-# calculate the adding and dropping of tips in order to
-# know the number of iterations ahead of running model
-add.bool.list <- calcAddBool (tree, birth, death,
-                              max.age = time + burnin,
-                              sample.unit = sample)
-# collect output for each interval
+# collect output for each sample point
 clade.performance <- list ()
+# calculate iterations
+iterations <- time/sample
 
 ## Model
 runmodel <- function (i) {
-  # get add bool
-  add.bool <- add.bool.list[[i]]
-  # grow tree using ERMM
-  tree <- growTree (add.bool, bias)
+  # grow tree using MRMM
+  tree <- growMRMMTree (birth = birth, death = death,
+                        stop.at = sample, stop.by = 'max.time',
+                        strength = strength, bias = bias)
   # write last tree to disk
   write.tree (tree, file = file.path (
-    res.dir, 'ERMM.tre'), append = TRUE)
+    res.dir, 'MRMM.tre'), append = TRUE)
   # calc 'success' of each node in tree
-  temp.success <- countChildren (tree)
+  temp.success <- .countChildren (tree, extinct)
   # add dataframe to a list
   clade.performance <<- c (clade.performance,
                           list (temp.success))
 }
-# remove any original tree in the folder
-if (file.exists (file.path (res.dir, 'ERMM.tre'))) {
-  file.remove (file.path (res.dir, 'ERMM.tre'))
-}
 # write seed tree first
-write.tree (tree, file = file.path (res.dir, 'ERMM.tre'),
-            append = TRUE)
+write.tree (tree, file = file.path (res.dir, 'MRMM.tre'))
 cat ('\nRunning tree growth model ...')
-m_ply (.data = (i = 1:length (add.bool.list)), .fun = runmodel,
+m_ply (.data = (i = 1:iterations), .fun = runmodel,
        .progress = 'time')
 # convert list of dataframes into single dataframe
 cat ('\nReformatting model output ...')
-res <- reformat (clade.performance, sample)
+res <- .reformat (clade.performance, sample)
 
 ## Saving results
 cat ('\nSaving results ...')
