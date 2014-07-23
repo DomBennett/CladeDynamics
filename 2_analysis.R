21cb6e7bf4c202690ccc5d49fe566c7ceb408eff
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-07-23 15:02:09 +0100

    Adapted for new model functions

--- 2_analysis.R
+++ 2_analysis.R
@@ -1,13 +1,12 @@
 ## 05/07/2014
 ## D.J. Bennett
-## Analysis of modelled rise and fall of clades
+## Analysis of modelled MRMM trees
+## This script is run through run.R
 
 ## Dirs
 # read in last run folder
-res.dir <- read.delim (file.path ('results', 'run_log.txt'),
-                       header = FALSE, stringsAsFactors = FALSE)[ ,1]
-#res.dir <- 'ts1000_int10_b1_d1_biasnone_date_seed2SunJul062014_time183238'
-res.dir <- file.path ('results', res.dir [length (res.dir)])
+res.dir <- read.csv (runlog, stringsAsFactors = FALSE)[1,'res.dir']
+res.dir <- file.path ('results', as.character (res.dir))
 
 ## Libraries
 source (file.path ('tools', 'analysis_tools.R'))
@@ -16,7 +15,7 @@ source (file.path ('tools', 'analysis_tools.R'))
 res <- read.csv (file = file.path (res.dir, 'clades_through_time.csv'))
 time.steps <- res[ ,1]
 res <- res[ ,-1]
-trees <- read.tree (file = file.path (res.dir, 'ERMM.tre'))
+trees <- read.tree (file = file.path (res.dir, 'MRMM.tre'))
 # remove burnin
 burnin.steps <- sum (burnin >= time.steps)
 time.steps <- time.steps[-(1:burnin.steps)]
@@ -35,9 +34,11 @@ pdf (file.path (res.dir, 'normalised_clade_success.pdf'))
 plotNormalisedSuccess (res, min.time.span, min.size)
 dev.off ()
 # Create .gif of trees produced
-cat ('\nPlotting tree growth ...')
-plotTreeGrowth (trees, file.path (res.dir, 'ERMM_tree.gif'),
-                time.steps)
+if (plot.tree.growth) {
+  cat ('\nPlotting tree growth ...')
+  plotTreeGrowth (trees, file.path (res.dir, 'MRMM_tree.gif'),
+                  time.steps)
+}
 # plot fate ~ ED
 cat ('\nPlotting Fate ~ ED ...')
 pdf (file.path (res.dir, 'fate_ED.pdf'))
