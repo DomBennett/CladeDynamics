5abed8439b57728a27f256a03d6b062e5b0fa8ce
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-07-25 16:28:28 +0100

    Rewrote calcTreeShapeStats, ref optional

--- tools/compare_tools.R
+++ tools/compare_tools.R
@@ -10,91 +10,66 @@ library (caper)
 library (geiger)
 library (ggplot2)
 
-calcTreeShapeStats <- function (tree) {
-  ## Calculates a variety of tree shape statistics and compares
-  ##  to stats generated for an equally sized Yule tree
-  ## Stats greater than 1 are greater than the Yule reference
-  calcImabalanceStats <- function (tree, ref.tree) {
-    # first convert trees to treeshape objects
-    ts.tree <- as.treeshape (tree)
-    ts.ref.tree <- as.treeshape (ref.tree)
-    # calculate toplogical measures of imbalance
-    # summed abs difference between sister clades
-    colless.stat <- colless (ts.tree)/colless (ts.ref.tree)
-    # summed number of ancestors for each tip
-    sackin.stat <- sackin (ts.tree)/sackin (ts.ref.tree)
-    # fusco test requires creation of a dataframe
-    tree$node.label <- NULL
-    tree.data <- data.frame (sp = tree$tip.label, nspp =
-                               rep (1, length (tree$tip.label)))
-    tree.data <- comparative.data (phy = tree, dat = tree.data,
-                                   names.col = sp)
-    tree.fusco.res <- fusco.test (tree.data, rich = nspp,
-                                  randomise.Iprime = FALSE)
-    ref.tree$node.label <- NULL
-    ref.tree.data <- data.frame (sp = ref.tree$tip.label, nspp =
-                                   rep (1, length (ref.tree$tip.label)))
-    ref.tree.data <- comparative.data (phy = ref.tree, dat = ref.tree.data,
-                                       names.col = sp)
-    ref.tree.fusco.res <- fusco.test (ref.tree.data, rich = nspp,
-                                      randomise.Iprime = FALSE)
-    iprime.stat <- tree.fusco.res$mean.Iprime/
-      ref.tree.fusco.res$mean.Iprime
-    list (colless.stat = colless.stat, sackin.stat = sackin.stat,
-          iprime.stat = iprime.stat)
-  }
-  calcShapeStats <- function (tree, ref.tree) {
-    # calculate tree branch based metrics
-    # set tree ages to 1
-    ref.tree.age <- getAge (ref.tree, node =
-                              length (ref.tree$tip.label) + 1)
-    ref.tree$edge.length <- ref.tree$edge.length/ref.tree.age
-    tree.age <- getAge (tree, node =
-                          length (tree$tip.label) + 1)
-    tree$edge.length <- tree$edge.length/tree.age
-    # measure of point of diversification
-    # measure difference for gamma stat
-    gamma.stat <- gammaStat (tree) - gammaStat (ref.tree)
-    # total cophenetic distance
-    tc.stat <- sum (cophenetic (tree))/sum (
-      cophenetic (ref.tree))
-    list (tc.stat = tc.stat, gamma.stat = gamma.stat)
-  }
-  # create reference Yule tree
-  ref.tree <- sim.bdtree (b = 1, d = 0, n = length (tree$tip.label))
-  # calculate imbalance metrics
-  if (is.binary.phylo (tree)[1] == TRUE) {
-    imbalance.stats <- calcImabalanceStats (tree, ref.tree)
-  } else {
-    # If tree is not binary create ...
-    imbalance.stats <- list (colless.stat = c (), sackin.stat = c (),
-                             iprime.stat = c ())
-    for (i in 1:100) {
-      # ... a random distribution of 100 trees ...
-      new.tree <- multi2di (tree)
-      temp.imbalance.stats <- calcImabalanceStats (new.tree, ref.tree)
-      imbalance.stats$colless.stat <- c (imbalance.stats$colless.stat,
-                                         temp.imbalance.stats$colless.stat)
-      imbalance.stats$sackin.stat <- c (imbalance.stats$sackin.stat,
-                                         temp.imbalance.stats$sackin.stat)
-      imbalance.stats$iprime.stat <- c (imbalance.stats$iprime.stat,
-                                         temp.imbalance.stats$iprime.stat)
+calcTreeShapeStats <- function (tree, reference = TRUE, n.dist = 100) {
+  ## Calculates a variety of tree shape statistics. If reference is TRUE
+  ##  compares to stats generated for an equally sized Yule tree
+  calcTopologyStats <- function (tree) {
+    calcStats <- function (tree) {
+      # first convert tree to treeshape object
+      ts.tree <- as.treeshape (tree)
+      # summed abs difference between sister clades
+      colless.stat <- colless (ts.tree)
+      # summed number of ancestors for each tip
+      sackin.stat <- sackin (ts.tree)
+      # fusco test requires creation of a dataframe
+      tree$node.label <- NULL
+      tree.data <- data.frame (sp = tree$tip.label, nspp =
+                                 rep (1, length (tree$tip.label)))
+      tree.data <- comparative.data (phy = tree, dat = tree.data,
+                                     names.col = sp)
+      tree.fusco.res <- fusco.test (tree.data, rich = nspp,
+                                    randomise.Iprime = FALSE)
+      iprime.stat <- tree.fusco.res$mean.Iprime
+      data.frame (colless.stat = colless.stat, sackin.stat = sackin.stat,
+            iprime.stat = iprime.stat)
     }
-    # ... and take the mean stats of these trees
-    imbalance.stats$colless.stat <- mean (imbalance.stats$colless.stat)
-    imbalance.stats$sackin.stat <- mean (imbalance.stats$sackin.stat)
-    imbalance.stats$iprime.stat <- mean (imbalance.stats$iprime.stat)
+    if (is.binary.phylo (tree)[1] == TRUE) {
+      return (unlist (calcStats (tree)))
+    } else {
+      # run multi2di n.dist times, calc stats and take means
+      run <- function (i) {
+        calcStats (multi2di (tree))
+      }
+      res <- mdply (.data = data.frame (i = 1:n.dist), .fun = run)[ ,-1]
+      return (colMeans (res))
+    }
   }
-  # calculate shape statistics
-  if (is.ultrametric (tree)) {
-    # if tree is ultrametric, calculate stats
-    shape.stats <- calcShapeStats (tree, ref.tree)
-  } else {
-    # else make NA
-    shape.stats <- list (gamma.stat = NA, tc.stat = NA)
+  calcBranchingStats <- function (tree) {
+    if (is.ultrametric (tree)) {
+      # set tree age to 1
+      tree.age <- getAge (tree, node = length (tree$tip.label) + 1)
+      tree$edge.length <- tree$edge.length/tree.age
+      # gamma stat
+      gamma.stat <- gammaStat (tree)
+      # total cophenetic distance
+      tc.stat <- sum (cophenetic (tree))
+    } else {
+      tc.stat <- gamma.stat <- NA
+    }
+    c ('tc.stat' = tc.stat, 'gamma.stat' = tc.stat)
   }
-  # return list of stats
-  c (imbalance.stats, shape.stats)
+  # calculate topology and branching stats
+  stats <- c (calcTopologyStats (tree), calcBranchingStats (tree))
+  if (reference) {
+    # Pure birth reference
+    reference <- sim.bdtree (b = 1, d = 0,
+                             n = length (tree$tip.label), stop = 'taxa')
+    ref.stats <- c (calcTopologyStats (reference),
+                    calcBranchingStats (reference))
+    # divide stats by ref stats
+    stats <- stats/ref.stats
+  }
+  as.list (stats)
 }
 
 calcMeanTreeShapeStats <- function (trees) {
