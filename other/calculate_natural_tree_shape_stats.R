5b7f6adac4d7796d007a5cb411dd42c0d055e2c0
Dominic Bennett <dominic.john.bennett@gmail.com>
2014-08-18 12:15:55 +0100

    Updated for large number of trees

--- other/calculate_natural_tree_shape_stats.R
+++ other/calculate_natural_tree_shape_stats.R
@@ -9,51 +9,85 @@ leeway <- 10
 ## Libraries
 source (file.path ('tools', 'compare_tools.R'))
 
+## Functions
+convertToDist <- function  (tree, n = 1) {
+  # Convert a polytmous tree into a distribution of dichotomous trees
+  trees <- list ()
+  for (i in 1:n) {
+    trees <- c (trees, list (multi2di (tree)))
+  }
+  class (trees) <- 'multiPhylo'
+  trees
+}
+addToList <- function (tree, trees, max.n, min.n) {
+  # Take tree and list, return list with right sized tree added
+  .resize <- function (tree) {
+    if (getSize (tree) >= max.n) {
+      # if the tree is bigger than target extract clades that are
+      #  within leeway of target
+      clade.trees <- getSubtrees (tree, min.n, max.n)
+      if (!is.null (clade.trees)) {
+        if (class (clade.trees) == 'multiPhylo') {
+          return (clade.trees)
+        } else {
+          return (list (clade.trees))
+        }
+      }
+    } else if (getSize (tree) >= min.n) {
+      # if not... just add it
+      return (list (tree))
+    }
+  }
+  # tree is a multiPhylo, run .add for each
+  if (class (tree) == 'multiPhylo') {
+    clade.trees <- list ()
+    for (t in tree) {
+      clade.trees <- c (clade.trees, .resize (t))
+    }
+    trees <- c (trees, list (clade.trees))
+  } else {
+    trees <- c (trees, .resize (tree))
+  }
+  trees
+}
+
 ## Dirs
 data.dir <- 'data'
 
 ## Input
-natural.trees <- list ()
-weights <- NULL # vector of source of tree, for weighting mean
-tree.files <- list.files (data.dir, '\\.tre')
-min.n <- target - (target*leeway/100)
-max.n <- target + (target*leeway/100)
+trees <- list () # hold sets of natural trees
+tree.files <- list.files (data.dir, '\\.tre') # list all .tre files in data folder
+min.n <- target - (target*leeway/100) # smallest ...
+max.n <- target + (target*leeway/100) # ... and biggest sizes for trees
 for (i in 1:length (tree.files)) {
   tree <- read.tree (file.path (data.dir, tree.files[i]))
-  # choose the biggest tree if list
+  # choose the biggest tree of the trees for a study
   if (class (tree) == 'multiPhylo') {
     sizes <- unlist(lapply (tree, getSize))
     tree <- tree[[which (sizes == max(sizes))[1]]]
   }
-  # if the tree is bigger than target extract clades that are
-  #  within leeway of target
-  if (getSize (tree) >= max.n) {
-    # may not work for polytomous trees, use try
-    clade.trees <- try (getSubtrees (tree, min.n, max.n), silent = TRUE)
-    if (is.null (clade.trees) | class (clade.trees) == 'try-error') {
-      next
-    }
-    for (clade.tree in clade.trees) {
-      natural.trees <- c (natural.trees, list (clade.tree))
-      weights <- c (weights, i)
-    }
-  } else if (getSize (tree) >= min.n) {
-    natural.trees <- c (natural.trees, list (tree))
-    weights <- c (weights, i)
-  } else {
-    next
+  # if polytomous, convert to a distribution
+  if (getSize (tree) != (tree$Nnode + 1)) {
+    tree <- convertToDist (tree)
   }
+  # add tree to list of trees
+  trees <- addToList (tree, trees, max.n, min.n)
 }
 
-## Calculate tree stats for each group of trees
+## Calculate tree stats for each set of trees
 colless.stat <- sackin.stat <- iprime.stat <- gamma.stat <- tc.stat <- NULL
-for (w in unique (weights)) {
-  natural.tree.stats <- calcTreeShapeStats (natural.trees[weights == w])
-  colless.stat <- c (colless.stat, natural.tree.stats['mean.colless.stat'][[1]])
-  sackin.stat <- c (sackin.stat, natural.tree.stats['mean.sackin.stat'][[1]])
-  iprime.stat <- c (iprime.stat, natural.tree.stats['mean.iprime.stat'][[1]])
-  gamma.stat <- c (gamma.stat, natural.tree.stats['mean.gamma.stat'][[1]])
-  tc.stat <- c (tc.stat, natural.tree.stats['mean.tc.stat'][[1]])
+for (set in trees) {
+  if (length (set) == 0) {
+    # if no trees met criteria, then set will be empty
+    next
+  }
+  stats <- calcTreeShapeStats (set) # calculate all stats
+  # extract the mean value of the set
+  colless.stat <- c (colless.stat, stats['mean.colless.stat'][[1]])
+  sackin.stat <- c (sackin.stat, stats['mean.sackin.stat'][[1]])
+  iprime.stat <- c (iprime.stat, stats['mean.iprime.stat'][[1]])
+  gamma.stat <- c (gamma.stat, stats['mean.gamma.stat'][[1]])
+  tc.stat <- c (tc.stat, stats['mean.tc.stat'][[1]])
 }
 natural.tree.stats <- data.frame (colless.stat, sackin.stat, iprime.stat,
                                   gamma.stat, tc.stat)
