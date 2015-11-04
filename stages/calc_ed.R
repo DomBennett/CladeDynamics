0ec508fb528b18ee5dd1fe6e7427ca1fedb50cd9
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-11-04 22:02:39 +0000

    Limit to 50-100 tip trees

--- stages/calc_ed.R
+++ stages/calc_ed.R
@@ -6,18 +6,27 @@ library (MoreTreeTools)
 
 # FUNCTIONS
 getrEDs <- function (treefiles, treefolder, ...) {
-  .calc <- function (treefile) {
+  .get <- function (treefile) {
+    .calc <- function (i) {
+      EDs <- calcED (trees[[i]])
+      rED <- EDs[ ,1] / sum (EDs[ ,1])
+      tip <- rownames (EDs)
+      data.frame (tip, rED, stringsAsFactors=FALSE)
+    }
     tree <- read.tree (file.path (treefolder, treefile))
     if (class (tree) == 'multiPhylo') {
       tree <- tree[[sample (1:length (tree), 1)]]
     }
-    EDs <- calcED (tree)
-    rED <- EDs[ ,1] / sum (EDs[ ,1])
-    tip <- rownames (EDs)
-    data.frame (tip, rED, stringsAsFactors=FALSE)
+    if (getSize (tree) > 50) {
+      trees <- getSubtrees (tree, min.n=50, max.n=100)
+    }
+    if (class (trees) == 'multiPhylo') {
+      l.data.calc <- data.frame (i=1:length (trees))
+      return (mdply (.data=l.data.calc, .fun=.calc)[ ,-1])
+    }
   }
-  l.data <- data.frame (treefile=treefiles)
-  mdply (.data=l.data, .fun=.calc, ...)
+  l.data.get <- data.frame (treefile=treefiles)
+  mdply (.data=l.data.get, .fun=.get, ...)
 }
 
 # EMPIRICAL
@@ -25,7 +34,10 @@ treefolder <- file.path ('data', 'parsed_trees_pathD8')
 meta <- read.csv (file.path (treefolder, 'treeinfo.csv'), stringsAsFactors=FALSE)
 # only pathD8 trees with fewer than 500 tips
 treefiles <- meta$filename[meta$rate.smooth == 'pathD8' & meta$ntaxa < 500 & !is.na (meta$ntaxa)]
-emp.rEDs <- getrEDs (treefiles, treefolder, .progress='time')
+pathD8.rEDs <- getrEDs (treefiles, treefolder, .progress='time')
+# only ultra trees at source
+treefiles <- meta$filename[meta$ultra]
+ultra.rEDs <- getrEDs (treefiles, treefolder, .progress='time')
 
 # Pan
 treefolder <- file.path ('results', 'pan')
@@ -43,26 +55,46 @@ treefiles <- list.files (treefolder, pattern='.tre')
 hyd.rEDs <- getrEDs (treefiles, treefolder, .progress='time')
 
 # Plot
-emp.rEDs$Scenario <- 'Emp.'
+pathD8.rEDs$Scenario <- 'Emp. (D8)'
+ultra.rEDs$Scenario <- 'Emp. (Ultra)'
 pan.rEDs$Scenario <- 'Pan'
 hyd.rEDs$Scenario <- 'Hyd'
 de.rEDs$Scenario <- 'DE'
-rEDs <- rbind (emp.rEDs, pan.rEDs, hyd.rEDs, de.rEDs)
-rEDs$Scenario <- factor (rEDs$Scenario, levels=c ('Emp.', 'Pan', 'Hyd', 'DE'))
-p <- ggplot (rEDs, aes (Scenario, rED))
+rEDs <- rbind (pathD8.rEDs, ultra.rEDs, pan.rEDs, hyd.rEDs, de.rEDs)
+rEDs$Scenario <- factor (rEDs$Scenario, levels=c ('Emp. (D8)', 'Emp. (Ultra)', 'Pan', 'Hyd', 'DE'))
+rEDs$log.rED <- log (rEDs$rED)
+p <- ggplot (rEDs, aes (Scenario, log.rED))
 p <- p + geom_violin (aes (fill=Scenario)) +
                         theme_bw () +
   theme (axis.text.x = element_blank(), axis.title.x = element_blank(),
-         text=element_text(size=25), legend.title=element_blank())
+         text=element_text(size=25), legend.title=element_blank()) +
+  ylab ('log (rED)')
 pdf (file.path ('results', 'figures', 'rED.pdf'))
 print (p)
 dev.off()
 
 # Print max for each
-cat ('Max Emp rED: [', max (emp.rEDs$rED, na.rm=TRUE), ']\n', sep='')
+cat ('Max D8 rED: [', max (pathD8.rEDs$rED, na.rm=TRUE), ']\n', sep='')
+cat ('Max ultra rED: [', max (ultra.rEDs$rED, na.rm=TRUE), ']\n', sep='')
 cat ('Max Pan rED: [', max (pan.rEDs$rED, na.rm=TRUE), ']\n', sep='')
 cat ('Max Hyd rED: [', max (hyd.rEDs$rED, na.rm=TRUE), ']\n', sep='')
 cat ('Max DE rED: [', max (de.rEDs$rED, na.rm=TRUE), ']\n', sep='')
 
 # Save
 save (rEDs, file=file.path ('results', 'rEDs.Rd'))
+
+# load
+load (file.path ('results', 'rEDs.Rd'))
+
+# Tree size matters
+# rEDs$ID <- paste0(rEDs$Scenario, rEDs$treefile, sep='-')
+# ntips <- table (rEDs$ID)
+# mean.log.rED <- tapply (rEDs$log.rED, rEDs$ID, mean)
+# rcols <- rainbow (length (unique (rEDs$Scenario)))
+# scenario.counts <- rowSums (table (rEDs$Scenario, rEDs$treefile) > 0)
+# cols <- NULL
+# for (i in 1:length (rcols)) {
+#   cols <- c (cols, rep (rcols[i], scenario.counts[i]))
+# }
+# plot (as.numeric (mean.log.rED) ~ log(as.numeric(ntips)), col=cols)
+# legend ('topright', legend=unique (rEDs$Scenario), col=rcols, pch=19)
