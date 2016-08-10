2431940457117a486d31cded530e3d6012d6591b
Dominic Bennett <dominic.john.bennett@gmail.com>
2016-08-10 17:03:08 +0100

    Minor correction

--- other/make_edbmm_plots.R
+++ other/make_edbmm_plots.R
@@ -4,13 +4,13 @@ library (gridExtra)
 
 data (EDBMMtrees) # load object
 # plot each for comparison
-p1<-chromatophylo (edbmm.trees[['DE']][['tree']],
+de_p<-chromatophylo (edbmm.trees[['DE']][['tree']],
                edge.cols=edbmm.trees[['DE']][['edge.diversity']],
                legend.title='Diversity', reduce.overlap=FALSE)
-p2<-chromatophylo (edbmm.trees[['Pan']][['tree']],
+pn_p<-chromatophylo (edbmm.trees[['Pan']][['tree']],
                edge.cols=edbmm.trees[['Pan']][['edge.diversity']],
                legend.title='Diversity', reduce.overlap=FALSE)
-p3<-chromatophylo (edbmm.trees[['Hyd']][['tree']],
+hyd_p<-chromatophylo (edbmm.trees[['Hyd']][['tree']],
                edge.cols=edbmm.trees[['Hyd']][['edge.diversity']],
                legend.title='Diversity', reduce.overlap=FALSE)
 # title settings
@@ -18,10 +18,10 @@ tt_theme <- theme(plot.title=element_text(hjust=0, vjust=1),
   legend.position='none', axis.title.x=element_blank(),
   axis.text.x=element_blank(), axis.ticks.x=element_blank(),
   text = element_text (size=25))
+nw_col <- scale_colour_gradient2(low="white", high="red")
 tiff(filename="/Users/djb208/Desktop/Figure_5.tiff",
-     width=1440, height=2880, units="px", pointsize=12)
-grid.arrange(p1 + ggtitle ('DE') + tt_theme,
-             p2 + ggtitle ('Hydra') + tt_theme,
-             p3 + ggtitle ('Pan') + tt_theme, ncol=1)
+     width=2880, height=1440, units="px", pointsize=12)
+grid.arrange(de_p + nw_col + ggtitle ('(DE)') + tt_theme,
+             hyd_p + nw_col + ggtitle ('(Hydra)') + tt_theme,
+             pn_p + nw_col + ggtitle ('(Pan)') + tt_theme, ncol=3)
 dev.off ()
-
